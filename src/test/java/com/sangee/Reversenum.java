a41a6a7bd1a19b8243adbc7ed2c50e39ec2858dd
SangeethaSana95 <sangeethasana95@gmail.com>
2025-10-18 21:04:25 +0530

    conflict

---
+++ src/test/java/com/sangee/Reversenum.java
@@ -0,0 +1,20 @@
+package com.sangee;
+
+public class Reversenum {
+	public static void main(String[] args)
+	{
+		
+	int num= 234;
+	int a,rev=0;
+	while(num>0)
+	{
+		a=num%10;
+		rev=rev*10+a;
+		num=num/10;
+	}
+	System.out.println(rev);
+		
+	}
+				
+
+}
