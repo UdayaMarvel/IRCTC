2d5fffbb1abfa058cdbeee58d700a73c7757e3d2
arvndtst <aravindoffici@gmail.com>
2025-10-22 22:24:19 +0530

    prac2

--- src/test/java/com/arvnd/TwoSum.java
+++ src/test/java/com/arvnd/TwoSum.java
@@ -2,19 +2,18 @@ package com.arvnd;
 
 public class TwoSum {
 	public static void main(String[] args) {
-		int[] nums = { 2, 7, 11, 15 };
+		int[] nums = {2,7,11,15};
 		int target = 9;
 //if the sum of two integer from the given array equals target,need to return those indices
-		for (int i = 0; i < nums.length; i++) {
-			for (int j = i + 1; j < nums.length; j++) {
-				if (nums[i] + nums[j] == target) {
-					System.out.println(i + "," + j);
+		for(int i=0;i<nums.length;i++) {
+			for(int j=i+1;j<nums.length;j++) {
+				if(nums[i]+nums[j]==target) {
+					System.out.println(i+","+j);
 					break;
-					
-				}
+					}
 			}
 		}
-
+		
 	}
 
 }
